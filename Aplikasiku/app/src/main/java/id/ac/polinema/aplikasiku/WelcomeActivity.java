d9b590f1f7992a1a173462a5089d6f9f962ecb93
afifahmn <afifahnugraheni5@gmail.com>
2019-09-17 14:08:37 +0700

    Membuat tampilan Login,Reset Password,Forgot Password,Success Login,Success Reset Password

--- Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeActivity.java
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/WelcomeActivity.java
@@ -5,13 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-	@Override
+		@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
+
 	}
 
 	public void clickLogin(View view) {
